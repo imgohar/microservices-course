e3131d39c538c768009d8cac8e510c65de4a3208
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2022-12-26 12:52:30 +0500

    26 v1

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -10,29 +10,29 @@ app.use(cors());
 const eventsArr = [];
 
 app.post('/events', (req, res) => {
-    const events = req.body;
+  const events = req.body;
 
-    eventsArr.push(events);
+  eventsArr.push(events);
 
-    axios.post('http://localhost:4000/events', {
-        events,
-    });
-    axios.post('http://localhost:4001/events', {
-        events,
-    });
-    axios.post('http://localhost:4002/events', {
-        events,
-    });
-    axios.post('http://localhost:4003/events', {
-        events,
-    });
-    res.send({ status: 'OK' });
+  axios.post('http://localhost:4000/events', {
+    events,
+  });
+  axios.post('http://localhost:4001/events', {
+    events,
+  });
+  axios.post('http://localhost:4002/events', {
+    events,
+  });
+  axios.post('http://localhost:4003/events', {
+    events,
+  });
+  res.send({ status: 'OK' });
 });
 
 app.get('/events', (req, res) => {
-    res.send(eventsArr);
+  res.send(eventsArr);
 });
 
 app.listen(4005, () => {
-    console.log('Posts server is spinning on port 4005');
+  console.log('Posts server is spinning on port 4005');
 });
