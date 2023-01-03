0babc5f25d3750fe36594a5e787334a8b637ca93
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2023-01-03 15:29:29 +0500

    auth complete

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 import Head from 'next/head';
+import buildClient from './api/build-client';
+import Header from '../components/header';
 
-export default function App({ Component, pageProps }) {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <>
       <Head>
@@ -11,7 +13,22 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </>
   );
-}
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
